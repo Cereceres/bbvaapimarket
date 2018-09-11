a47ee98965b8ed7ff81397a711bc6f226f9d4cc2
cereceres <jesus.edelcereceres@gmail.com>
2018-09-11 13:15:39 -0600

    the engine is setted

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const promisify = require('fying-promises');
 
+const card = require('./lib/card');
+
 const post = promisify(request.post.bind(request));
 const get = promisify(request.get.bind(request));
 
@@ -163,38 +165,5 @@ module.exports = {
         })
             .then(([ { body } ]) => body)
     },
-    card: {
-        getMeCard: (token) => get({
-            baseUrl: 'https://apis.bbva.com/cards/v2',
-            uri: '/me/cards',
-            headers: {
-                'Authorization': `jwt ${ token }`,
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(([ { body } ]) => body),
-        getMeCardDetails: (token, cardId) => get({
-            baseUrl: 'https://apis.bbva.com/cards/v2',
-            uri: `/me/accounts${cardId ? `/${cardId}` : ''}`,
-            headers: {
-                'Authorization': `jwt ${ token }`,
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(([ { body } ]) => body),
-        getCardTransactions:(token, cardId) => get({
-            baseUrl: 'https://apis.bbva.com/accounts/v1',
-            uri: `/me/accounts${cardId ? `/${cardId}` : ''}/transactions`,
-            headers: {
-                'Authorization': `jwt ${ token }`,
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(([ { body } ]) => body)
-    }
-
-
+    card: {...card}
 };
